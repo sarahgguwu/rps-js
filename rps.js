8d2f899ef1d0f789123787117e57f3c8f26e0e32
Sarah Fayyaz <git@imsarah.ca>
2022-10-02 21:11:40 -0400

    fixed code

--- rps.js
+++ rps.js
@@ -1,9 +1,45 @@
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
-    computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-    computerChoice = "paper";
-} else {
-    computerChoice = "scissors";
-}
+function rpsFuction() {
+    var userSelection = prompt("Choose ROCK, PAPER, or SCISSORS?");
+    var computerSelection = Math.random();
+    if (computerSelection < 0.34) {
+        computerSelection = "rock";
+    } else if(computerSelection <= 0.67) {
+        computerSelection = "paper";
+    } else {
+        computerSelection = "scissors";
+    }
+    
+    var compare = function(userSelection1, computerSelection1) {
+        if(userSelection1 === computerSelection1) {
+          alert("It's a tie!");
+    }
+    if(userSelection1 === "rock") {
+        if(computerSelection1 === "scissors") {
+            alert("User wins");
+        } else {
+            alert("Computer wins");
+        }
+    }
+    if(userSelection1 === "paper") {
+        if(computerSelection1 === "rock") {
+            alert("User wins");
+        } else {
+            if(computerSelection1 === "scissors") {
+                alert("Computer wins");
+        }
+    }
+    if(userSelection1 === "scissors") {
+        if(computerSelection1 === "rock") {
+            alert("Computer wins");
+        } else {
+            if(computerSelection1 === "paper") {
+               alert("User wins");
+            }
+        }
+    }
+    }
+    };
+    console.log("User Choice: " + userSelection);
+    console.log("Computer Choice: " + computerSelection);
+    compare(userSelection, computerSelection)
+    } 
